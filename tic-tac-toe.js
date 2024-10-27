f339ee9ff1125a0a4f91d42ed20fd04eae87bdac
Arii07 <williamsariel68@gmail.com>
2024-10-26 22:33:43 -0400

    Added functionality to alternate X and O on click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  const squares = document.querySelectorAll("#board div");
-  squares.forEach(square => square.classList.add("square"));
-});
+    const squares = document.querySelectorAll("#board div");
+    squares.forEach(square => square.classList.add("square"));
+  });
+  
+ 
+  let currentPlayer = "X";
+  
+  document.querySelectorAll("#board div").forEach(square => {
+    square.addEventListener("click", () => {
+      if (square.textContent === "") {
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+      }
+    });
+  });
+  
