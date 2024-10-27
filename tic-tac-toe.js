5620aa5b9c43792bc177adae48ee956804d98f6a
Arii07 <williamsariel68@gmail.com>
2024-10-26 22:36:37 -0400

    Added hover effect for squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -17,3 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
+ 
+  document.querySelectorAll("#board div").forEach(square => {
+    square.addEventListener("mouseover", () => square.classList.add("hover"));
+    square.addEventListener("mouseout", () => square.classList.remove("hover"));
+  });
+  
